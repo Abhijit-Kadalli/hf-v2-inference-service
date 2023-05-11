a6cba6ab0eccb697c5792f4eb2163a227a67f59d
Abhijit Kadalli <abhijitkadalli@nitgoa.ac.in>
2023-05-11 19:22:37 +0530

    fixed args inputs

--- convert.py
+++ convert.py
@@ -1,11 +1,65 @@
-from mlserver.types import MetadataTensor
 from fastapi import HTTPException
 import PIL.Image
 import json
+from pydantic import BaseModel as _BaseModel
+from typing import Any, Dict, List, Optional
+
+#WILL ONLY WORK WITH LINUX
+#from mlserver.types.dataplane import MetadataTensor
+
+from pydantic import Extra, Field
+
+
+class BaseModel(_BaseModel):
+    """
+    Override Pydantic's BaseModel class to ensure all payloads exclude unset
+    fields by default.
+
+    From:
+        https://github.com/pydantic/pydantic/issues/1387#issuecomment-612901525
+    """
+
+    def dict(self, exclude_unset=True, exclude_none=True, **kwargs):
+        return super().dict(
+            exclude_unset=exclude_unset, exclude_none=exclude_none, **kwargs
+        )
+
+    def json(self, exclude_unset=True, exclude_none=True, **kwargs):
+        return super().json(
+            exclude_unset=exclude_unset, exclude_none=exclude_none, **kwargs
+        )
+
+class Parameters(BaseModel):
+    class Config:
+        extra = Extra.allow
+
+    content_type: Optional[str] = None
+    headers: Optional[Dict[str, Any]] = None
+
+class MetadataTensor(BaseModel):
+    name: str
+    datatype: str
+    shape: List[int]
+    parameters: Optional["Parameters"] = None
+
+# class MetadataTensor:
+#     def __init__(
+#         self,
+#         name: str,
+#         datatype: str,
+#         shape: List[int],
+#         parameters: Optional[dict] = None,
+#         data: Optional[list] = None,
+#     ):
+#         self.name = name
+#         self.datatype = datatype
+#         self.shape = shape
+#         self.parameters = parameters
+#         self.data = data
 
 def convert_to_v2_input(hf_pipeline, input_data):
     if hf_pipeline == "zero-shot-classification":
-        input_data = json.loads(input_data)
+        #input_data = json.loads(input_data)
         v2_input = {
             "inputs": [
                 MetadataTensor(
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import argparse
 import requests
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 import uvicorn
 from convert import convert_to_v2_input
 
+
 app = FastAPI()
 
-
-# class HFInput(BaseModel):
-#     inputs: dict
-
+parser = argparse.ArgumentParser()
+parser.add_argument("--hf_pipeline", type=str, help="Hugging Face pipeline name")
+parser.add_argument("--model_deployed_url", type=str, help="URL of the deployed model")
+args = parser.parse_args()
+    
+# Set the pipeline name and model URL from the command-line arguments
+pipeline_name = args.hf_pipeline
+model_deployed_url = args.model_deployed_url
 
 def forward_request_to_model(model_deployed_url, v2_input):
+    print("Forwarding request to model..." + model_deployed_url)
     try:
         response = requests.post(model_deployed_url, json=v2_input)
         response.raise_for_status()
@@ -22,20 +27,15 @@ def forward_request_to_model(model_deployed_url, v2_input):
 
 
 @app.post("/")
-def predict(hf_pipeline: str, model_deployed_url: str, input_data):
-    v2_input = convert_to_v2_input(hf_pipeline, input_data)
+def predict(inputs: dict):
+    print("Received request for pipeline: ")
+    print(pipeline_name)
+    print("Received inputs: " + str(inputs))
+    v2_input = convert_to_v2_input(pipeline_name, inputs)
+    print("Converted to V2 input: " + str(v2_input))
     response = forward_request_to_model(model_deployed_url, v2_input)
     return response
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--hf_pipeline", type=str, help="Hugging Face pipeline name")
-    parser.add_argument("--model_deployed_url", type=str, help="URL of the deployed model")
-    args = parser.parse_args()
-
-    # Set the pipeline name and model URL from the command-line arguments
-    pipeline_name = args.hf_pipeline
-    model_deployed_url = args.model_deployed_url
-
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
