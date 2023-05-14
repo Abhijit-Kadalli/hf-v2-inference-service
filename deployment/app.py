5185a634b08ef4cc7ead866b4f7296dd4d4ebff4
Abhijit Kadalli <abhijitkadalli@nitgoa.ac.in>
2023-05-15 01:31:26 +0530

    updated deployment reqs

--- deployment/app.py
+++ deployment/app.py
@@ -36,4 +36,4 @@ def predict(inputs: dict):
     return response
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
